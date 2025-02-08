a774b3a9503813c241840829836e73dbadfd3100
Lokeswar <lokeswar5656@gmail.com>
2025-02-08 17:15:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # Import CORS
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://lokeswar777.github.io"}})  # Allow only your GitHub Pages domain
 
-# Load the model and data
+# Load model and data
 model = joblib.load('models/gold_price_model.pkl')
 data = pd.read_csv('data/cleaned_gold_price_dataset.csv')
-latest_close_price = data['close_price'].iloc[-1]
 
-# Predict the next day's price
+latest_close_price = data['close_price'].iloc[-1]
 predicted_price = model.predict([[float(latest_close_price)]])[0]
 
 @app.route('/predict', methods=['GET'])
